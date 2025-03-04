d0cce612f528a73459a19773d5c169af1cd1ede3
sojicb <bogoljub.sojic987@gmail.com>
2025-03-04 23:13:47 +0100

    lsp

---
+++ LSP/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class BankAccount
+{
+	public decimal Balance { get; protected set; }
+
+	public BankAccount(decimal initialBalance)
+	{
+		Balance = initialBalance;
+	}
+
+	// General withdrawal method for BankAccount
+	public virtual void Withdraw(decimal amount)
+	{
+		if (Balance >= amount)
+		{
+			Balance -= amount;
+			Console.WriteLine($"Withdrew {amount}, balance is now {Balance}");
+		}
+		else
+		{
+			Console.WriteLine("Insufficient funds!");
+		}
+	}
+}
+
+public class CheckingAccount : BankAccount
+{
+	public decimal OverdraftLimit { get; set; }
+
+	public CheckingAccount(decimal initialBalance, decimal overdraftLimit)
+		: base(initialBalance)
+	{
+		OverdraftLimit = overdraftLimit;
+	}
+
+	// CheckingAccount allows withdrawal beyond the balance, up to the overdraft limit
+	public override void Withdraw(decimal amount)
+	{
+		if (Balance + OverdraftLimit >= amount)
+		{
+			Balance -= amount;
+			Console.WriteLine($"Withdrew {amount}, balance is now {Balance} (within overdraft limit)");
+		}
+		else
+		{
+			Console.WriteLine("Exceeded overdraft limit!");
+		}
+	}
+}
+
+public class SavingsAccount : BankAccount
+{
+	public SavingsAccount(decimal initialBalance) : base(initialBalance)
+	{
+	}
+
+	// SavingsAccount doesn't allow withdrawal beyond balance
+	public override void Withdraw(decimal amount)
+	{
+		if (Balance >= amount)
+		{
+			Balance -= amount;
+			Console.WriteLine($"Withdrew {amount}, balance is now {Balance}");
+		}
+		else
+		{
+			Console.WriteLine("Insufficient funds!");
+		}
+	}
+}
---
+++ LSP/Program.cs
@@ -0,0 +1,20 @@
+﻿
+BankAccount checkingAccount = new CheckingAccount(1000, 500);
+BankAccount savingsAccount = new SavingsAccount(1000);
+
+// Here, we're expecting the Liskov Substitution Principle to hold, but it doesn't:
+checkingAccount.Withdraw(1200);  // This works for CheckingAccount, but not for SavingsAccount
+savingsAccount.Withdraw(1200);   // This will fail for SavingsAccount, but should work for CheckingAccount
+
+Console.WriteLine("Testing substitution with both types of accounts...");
+
+// Using both accounts interchangeably may lead to unexpected results
+TestWithdrawal(checkingAccount);
+TestWithdrawal(savingsAccount);
+	
+
+static void TestWithdrawal(BankAccount account)
+{
+	// This should work correctly for a CheckingAccount but may not work correctly for a SavingsAccount
+	account.Withdraw(500);
+}
