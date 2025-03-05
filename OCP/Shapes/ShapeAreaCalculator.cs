186c67d901c70e282e8687a39acc0211e07bc770
AnaJurišić <AnaJurii@AJURISIC-INT>
2025-03-05 16:00:42 +0100

    [Added] Dependency Injection for ShapeAreaCalculator

---
+++ OCP/Shapes/ShapeAreaCalculator.cs
@@ -0,0 +1,24 @@
+﻿using OCP.Shape;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP.Shapes
+{
+    public class ShapeAreaCalculator
+    {
+        private readonly IShape _shape;
+
+        public ShapeAreaCalculator(IShape shape)
+        {
+            _shape = shape;
+        }
+
+        public string DisplayArea()
+        {
+            return $"Area of {_shape.GetType().Name.ToString().ToLower()}: {_shape.CalculateArea()}";
+        }
+    }
+}
