3ea175e54ba4e4f7b927f16282b130dc4a52d04b
AnaJurišić <AnaJurii@AJURISIC-INT>
2025-03-05 13:37:23 +0100

    [Changed] Files to follow OCP nad SRP principles

---
+++ OCP/Shapes/IShape.cs
@@ -0,0 +1,7 @@
+﻿namespace OCP.Shape
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+}
---
+++ OCP/Shapes/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using OCP.Shape;
+
+namespace OCP.Shapes
+{
+    class Triangle : IShape
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public double CalculateArea()
+        {
+            return 0.5 * this.Base * this.Height;
+        }
+    }
+}
